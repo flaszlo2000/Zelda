3263f424455f77208e253a47c14e5c58f10505a4
flaszlo2000 <f.laszlo2000@gmail.com>
2022-02-12 23:25:15 +0100

    indentation fix

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,20 +7,20 @@ from scripts.image_provider import image_provider
 
 
 def import_csv_layout(path):
-	terrain_map = []
-	with open(path) as level_map:
-		layout = reader(level_map,delimiter = ',')
-		for row in layout:
-			terrain_map.append(list(row))
-		return terrain_map
+    terrain_map = []
+    with open(path) as level_map:
+        layout = reader(level_map,delimiter = ',')
+        for row in layout:
+            terrain_map.append(list(row))
+        return terrain_map
 
 def import_folder(path):
-	surface_list = []
+    surface_list = []
 
-	for _,__,img_files in walk(path):
-		for image in img_files:
-			full_path = path + '/' + image
-			image_surf = image_provider.provideWithAlphaConvert(Path(full_path))
-			surface_list.append(image_surf)
+    for _,__,img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
+            image_surf = image_provider.provideWithAlphaConvert(Path(full_path))
+            surface_list.append(image_surf)
 
-	return surface_list
+    return surface_list
--- src/main.py
+++ src/main.py
@@ -6,70 +6,70 @@ from level import Level
 
 
 class Game:
-	def __init__(self):
-		self.alive = False
+    def __init__(self):
+        self.alive = False
 
-		pygame.init()
-		self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGTH))
-		pygame.display.set_caption('Zelda')
-		self.clock = pygame.time.Clock()
+        pygame.init()
+        self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGTH))
+        pygame.display.set_caption('Zelda')
+        self.clock = pygame.time.Clock()
 
-		self.level = Level() # TODO: make this dynamic
+        self.level = Level() # TODO: make this dynamic
 
-		self._fetchBindings()
+        self._fetchBindings()
 
-		# sound 
-		main_sound = pygame.mixer.Sound('./audio/main.ogg')
-		main_sound.set_volume(0.5)
-		main_sound.play(loops = -1)
+        # sound 
+        main_sound = pygame.mixer.Sound('./audio/main.ogg')
+        main_sound.set_volume(0.5)
+        main_sound.play(loops = -1)
 
-	def _fetchBindings(self) -> None:
-		# TODO: read this from file
-		self.event_dict = {
-			pygame.QUIT: self._quit
-		}
+    def _fetchBindings(self) -> None:
+        # TODO: read this from file
+        self.event_dict = {
+            pygame.QUIT: self._quit
+        }
 
-		self.key_binding_dict = {
-			pygame.K_m: self.level.toggle_menu,
-			pygame.K_ESCAPE: self.showMenu
-		}
+        self.key_binding_dict = {
+            pygame.K_m: self.level.toggle_menu,
+            pygame.K_ESCAPE: self.showMenu
+        }
 
-	def _quit(self) -> None:
-		pygame.quit()
-		sys.exit()
+    def _quit(self) -> None:
+        pygame.quit()
+        sys.exit()
 
-	def showMenu(self) -> None:
-		print("show menu")
+    def showMenu(self) -> None:
+        print("show menu")
 
-	def run(self):
-		self.alive = True
+    def run(self):
+        self.alive = True
 
-		handled_events = self.event_dict.keys()
-		key_bindings = self.key_binding_dict.keys()
+        handled_events = self.event_dict.keys()
+        key_bindings = self.key_binding_dict.keys()
 
-		while self.alive:
-			for event in pygame.event.get():
-				if event.type in handled_events:
-					self.event_dict[event.type]()
-				
-				if event.type == pygame.KEYDOWN and event.key in key_bindings:
-					self.key_binding_dict[event.key]()
+        while self.alive:
+            for event in pygame.event.get():
+                if event.type in handled_events:
+                    self.event_dict[event.type]()
+                
+                if event.type == pygame.KEYDOWN and event.key in key_bindings:
+                    self.key_binding_dict[event.key]()
 
-			self.screen.fill(settings.WATER_COLOR)
-			self.level.run()
-			pygame.display.update()
-			self.clock.tick(settings.FPS)
+            self.screen.fill(settings.WATER_COLOR)
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(settings.FPS)
 
-	def sigint(self, signum, frame) -> None:
-		self.alive = False
-	
-	def sigterm(self, signum, frame) -> None:
-		self.alive = False
+    def sigint(self, signum, frame) -> None:
+        self.alive = False
+    
+    def sigterm(self, signum, frame) -> None:
+        self.alive = False
 
  
 if __name__ == '__main__':
-	game = Game()
-	signal.signal(signal.SIGINT, game.sigint)
-	signal.signal(signal.SIGTERM, game.sigterm)
+    game = Game()
+    signal.signal(signal.SIGINT, game.sigint)
+    signal.signal(signal.SIGTERM, game.sigterm)
 
-	game.run()
+    game.run()
