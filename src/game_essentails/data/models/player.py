c47bdadbebb728201f0497c1723bef989fb5d3a4
flaszlo2000 <f.laszlo2000@gmail.com>
2022-04-30 21:07:49 +0200

    fixed player model issubclass error

--- src/game_essentails/data/models/player.py
+++ src/game_essentails/data/models/player.py
@@ -12,7 +12,7 @@ class StatData:
     should_be_shown: Optional[bool] = field(default = None)
 
     def __post_init__(self) -> None:
-        self.should_be_shown = self.base == self.max
+        self.should_be_shown = self.base != self.max
 
 @dataclass
 class PlayerData(GameData):
@@ -26,7 +26,18 @@ class PlayerData(GameData):
     speed: StatData
 
     def __post_init__(self) -> None:
-        # NOTE: at the loading stage 
+        # NOTE: at the data loading stage StatData is parsed as dict so I have to convert it
+
+        print(self.__annotations__)
         for param, value in self.__dict__.items():
-            if isinstance(value, dict):
-                self.__dict__[param] = StatData(**value) # type: ignore
+            try:
+                # we don't want to convert all of the dict like objects, only StatData
+                subclass_check = not issubclass(dict, self.__annotations__[param])
+            except (TypeError, KeyError):
+                # NOTE: issubclass throws "TypeError: Subscripted generics cannot be used with class and instance checks"
+                # error if self.__annotations__[param] is a kind of typing type (like Dict[str, str])
+                # KeyError could be thrown because of the inheritance
+                continue
+            else:
+                if isinstance(value, dict) and subclass_check:
+                    self.__dict__[param] = StatData(**value) # type: ignore
